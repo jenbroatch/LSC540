70aaa75e6ecba26348169af7b2adf96ff066960d
jenbroatch <54128714+jenbroatch@users.noreply.github.com>
2023-08-25 16:32:54 -0700

    Add files via upload

---
+++ RScripts/Module4Script.R
@@ -0,0 +1,67 @@
+#Confidence Interval Example
+.099*(1-.099)/1432 
+
+install.packages("binom", dependencies = TRUE) # only if not yet installed
+library(binom) 
+.099*1432 
+binom.confint(142, n = 1432)
+
+binom.confint(142, n = 1432, conf.level=.95, methods="asymptotic")
+binom.confint(142, n = 1432, conf.level=.99, methods="asymptotic")
+binom.confint(142, n = 1432, conf.level=.90, methods="asymptotic")
+
+
+X=0.099*1432000
+binom.confint(X, n = 1432000, conf.level=.95, methods="asymptotic")
+
+library(samplingbook)
+sample.size.prop(0.01, P = 0.97, N = Inf, level = 0.95) 
+
+
+Flintdata<- read.csv("https://raw.githubusercontent.com/jenbroatch/LSC540/main/DataSets/Flint.csv")
+View(Flintdata)
+counts<- table(Flintdata$Contaminated) 
+
+barplot(counts, main="Flint Water Contamination",
+        xlab="Number of Contaminated Homes") 
+barplot(counts, main="Flint Water Contamination",
+        xlab="Number of Contaminated Homes", names.arg=c("No Lead Contamination Evident", 
+                                                         "Lead Contamination Evident"), 
+        col=c("red", "blue"))
+
+
+prop.test(54, 271, p = 0.10, alternative = "greater")
+
+prop.test(25, 271, p = 0.10, alternative = "greater")
+
+#No Continuity Correction
+prop.test(54, 271, p = 0.10, alternative = "greater", correct=F)
+
+Teststat=((54/271)-.1)/sqrt(.1*.9/271)
+Teststat
+
+#Note that the square of a variable with a standard normal distribution 
+#is a Chi-squared distribution.
+Teststat^2
+
+
+heart <- read.csv("C:/Users/jenni/Dropbox (ASU)/Courses/LSC540/Data Sets/heart.csv") 
+View(heart)
+library(dplyr)
+library(ggplot2)
+
+#Contingency Table 
+my.ctable <- table(heart$ExerciseAngina, heart$HeartDisease,dnn=c("Heart Disease (1=yes, 0=no)","Exercise Angina 1=yes,0=no" ))
+my.ctable
+addmargins(my.ctable)
+#Side by Side Bar Graph
+heart %>% 
+  ggplot( aes(x = ExerciseAngina, fill =as.factor(HeartDisease) )) + 
+  geom_bar(position="dodge") + 
+  labs(
+    title = "Side by Side Bar Graph", 
+    x = "Exercise Angina", 
+    fill= "Heart Disease (1=Yes, 0=No)"
+  ) + 
+  theme_bw() 
+prop.test(table(heart$HeartDisease, heart$ExerciseAngina))  
---
+++ RScripts/Module6_Regression.R
@@ -0,0 +1,42 @@
+#Module 6 
+#Regression 
+reg1=lm(gestation$Longevity~gestation$Gestational.Period )
+plot(gestation$Gestational.Period, gestation$Longevity, xlab="Gestational Period (days)", ylab="Longevity (years)")
+abline(reg1)
+summary(reg1)
+
+library(Lock5Data)
+View(AllCountries)
+AllCountries1 <- AllCountries %>% 
+  filter (!is.na(Developed))
+AllCountries1$Developed<- as.factor(AllCountries1$Developed)
+
+plot( AllCountries1$Hunger, AllCountries1$LifeExpectancy, xlab="% of the population considered undernourished", ylab="Life Expectancy (years)"  )
+
+
+ggplot(data = AllCountries1, aes(x =LifeExpectancy)) + 
+  geom_histogram() + 
+  ggtitle("Distribution of LifeExpectancy") +
+  xlab("LifeExpectancy (in years)")
+reg1<- lm(AllCountries1$LifeExpectancy~AllCountries1$Hunger)
+summary(reg1)
+plot(reg1, 2)
+
+
+
+
+plot( AllCountries1$GDP, AllCountries1$LifeExpectancy, xlab="GDP", ylab="Life Expectancy (years)"  )
+plot( log(AllCountries1$GDP), AllCountries1$LifeExpectancy,  xlab="ln(GDP)", ylab="Life Expectancy (years)"  )
+reg2<- lm(AllCountries1$LifeExpectancy~AllCountries1$Hunger+ log(AllCountries1$GDP))
+summary(reg2)
+
+ggplot(AllCountries1, aes(x=as.factor(Developed), y=LifeExpectancy)) + geom_boxplot() +
+  ggtitle("Distribution Average Life expectancy by Development") +
+  ylab("LifeExpectancy (years)") + 
+  xlab("Categories for kilowatt hours per capita, 1= under 2500, 2=2500 to 5000, 3=over 5000") 
+
+reg3<-lm(AllCountries1$LifeExpectancy~AllCountries1$Hunger+ log(AllCountries1$GDP)+AllCountries1$Developed)
+summary(reg3)
+
+reg4<-lm(AllCountries1$LifeExpectancy~AllCountries1$Hunger+ AllCountries1$Developed)
+summary(reg4)
